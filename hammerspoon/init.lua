2d8dac85acb87658f18dfe46c85d95b9c0bf5831
DaeBum <48302257+choidabom@users.noreply.github.com>
2023-10-18 02:02:38 +0000

    add. hammerspoon init.lua

---
+++ hammerspoon/init.lua
@@ -0,0 +1,35 @@
+local keyMap = {
+    C = "Google Chrome",
+    V = "Visual Studio Code",
+    F = "Finder",
+    P = "Postman",
+    S = "Slack",
+    T = "iTerm",
+    N = 'notes',
+    K = 'kakaoTalk',
+}
+
+for k, prog in pairs(keyMap) do
+    hs.hotkey.bind({'cmd', 'ctrl'}, k, function()
+        print(k, prog)
+        hs.application.launchOrFocus(prog)
+    end)
+end
+
+hs.hints.hintChars = {'f','j','d','k','s','l','r','u','e','i','w','v','m'}
+hs.hotkey.bind({'ctrl','cmd'}, 'return', function()
+    hs.hints.windowHints(hs.window.filter.new():getWindows(hs.window.filter.sortByFocused))
+end)
+
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(100)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
